67db3b14426d86548cea2a3a472534391eb0cfb8
蔡明师 <cmsax@live.com>
2019-10-10 13:36:04 +0800

    finish

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-pyinstaller --name="ci-demo" --windowed -F main.py
+# on macOS, system python2.7 is used.
+case "${TRAVIS_OS_NAME}" in
+    osx)
+        cp -vf /usr/local/lib/python2.7/site-packages/shiboken2/libshiboken2-python2.7v.5.13.dylib /usr/local/lib/python2.7/site-packages/PySide2/
+        cp -vf /usr/local/lib/python2.7/site-packages/shiboken2/libshiboken2-python2.7v.5.13.dylib /usr/local/lib/python2.7/site-packages/PyInstaller/hooks/
+        pyinstaller --name="pyqt-ci-demo" --windowed -F --icon ./icon.icns --osx-bundle-identifier pyqt-ci-demo main.py
+        ;;
+    windows)
+        pyinstaller --name="pyqt-ci-demo" --windowed -F main.py
+        ;;
+esac
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 # -*- coding: utf-8 -*-
+# File: ci-demo/main.py
+# Author: MingshiCai i@unoiou.com
+# Created Date: 2019-10-09 23:52:29
+# ----
+# Last Modified:
+# Modified By:
+# ----
+# Copyright (c) 2019 MingshiCai i@unoiou.com
 import sys
 
 from PySide2 import QtCore, QtWidgets, QtGui
